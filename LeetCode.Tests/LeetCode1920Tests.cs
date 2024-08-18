8408c40a50cfcabb336a9f8025cfe93c40ea871d
Patryk Lewon <patryklewonn@gmail.com>
2024-08-18 20:39:51 +0200

    problem 1920

---
+++ LeetCode.Tests/LeetCode1920Tests.cs
@@ -0,0 +1,19 @@
+﻿using FluentAssertions;
+
+namespace LeetCode.Tests;
+
+[TestFixture]
+public class LeetCode1920Tests
+{
+    [TestCase(new[] { 0, 2, 1, 5, 3, 4 }, new[] { 0, 1, 2, 4, 5, 3 })]
+    [TestCase(new[] { 5, 0, 1, 2, 3, 4 }, new[] { 4, 5, 0, 1, 2, 3 })]
+    public void BuildArray_ReturnExpected(int[] input, int[] expected)
+    {
+        // Arrange
+        // Act
+        var result = LeetCode1920.BuildArray(input);
+        
+        // Assert
+        result.Should().BeEquivalentTo(expected);
+    }
+}
---
+++ LeetCode/LeetCode1920.cs
@@ -0,0 +1,16 @@
+﻿namespace LeetCode;
+
+public static class LeetCode1920
+{
+    public static int[] BuildArray(int[] nums)
+    {
+        Span<int> output = stackalloc int[nums.Length];
+
+        for (var i = 0; i < nums.Length; i++)
+        {
+            output[i] = nums[nums[i]];
+        }
+
+        return output.ToArray();
+    }
+}
