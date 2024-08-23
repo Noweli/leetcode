7799f0fcda8e7cb6a97572c7f621a7cb2a5f3e7a
Patryk Lewon <patryklewonn@gmail.com>
2024-08-23 21:27:55 +0200

    Problem 2181

---
+++ LeetCode.Tests/LeetCode2181Tests.cs
@@ -0,0 +1,23 @@
+﻿using FluentAssertions;
+using LeetCode.Tests.Helpers;
+
+namespace LeetCode.Tests;
+
+[TestFixture]
+public class LeetCode2181Tests
+{
+    [TestCase(new[] { 0, 3, 1, 0, 4, 5, 2, 0 }, new[] { 4, 11 })]
+    [TestCase(new[] { 0, 1, 0, 3, 0, 2, 2, 0 }, new[] { 1, 3, 4 })]
+    public void MergeNodes_ReturnExpected(int[] values, int[] expected)
+    {
+        // Arrange
+        var list = ListNodeHelpers.CreateListNodeWithValues(values);
+        
+        // Act
+        var solutionResult = LeetCode2181.MergeNodes(list);
+        var result = ListNodeHelpers.ReadListNode(solutionResult);
+
+        // Assert
+        result.Should().BeEquivalentTo(expected);
+    }
+}
---
+++ LeetCode/LeetCode2181.cs
@@ -0,0 +1,95 @@
+﻿namespace LeetCode;
+
+
+public static class LeetCode2181
+{
+    public static SharedModels.ListNode MergeNodes(SharedModels.ListNode head)
+    {
+        var root = new SharedModels.ListNode();
+        var currentNode = root;
+
+        var currentSearchNode = head.next;
+        while (true)
+        {
+            if (currentSearchNode?.next is null)
+            {
+                break;
+            }
+            
+            if(currentSearchNode.val == 0)
+            {
+                var newNode = new SharedModels.ListNode();
+                currentNode.next = newNode;
+                currentNode = newNode;
+                currentSearchNode = currentSearchNode.next;
+                
+                continue;
+            }
+
+            currentNode.val += currentSearchNode.val;
+            currentSearchNode = currentSearchNode.next;
+        }
+
+        return root;
+    }
+    
+    // Did not pass due to timeout
+    public static SharedModels.ListNode MergeNodesApproach1(SharedModels.ListNode head)
+    {
+        var numbers = new List<int>();
+        var merged = new List<int>();
+        var values = ReadNodeList(head);
+        
+        foreach (var nodeValue in values.Skip(1))
+        {
+            if (nodeValue == 0)
+            {
+                merged.Add(numbers.Sum());
+                numbers.Clear();
+                continue;
+            }
+
+            numbers.Add(nodeValue);
+        }
+
+        return CreateListNodeWithValues(merged.ToArray());
+    }
+
+    private static int[] ReadNodeList(SharedModels.ListNode listNode)
+    {
+        List<int> output = [listNode.val];
+
+        if (listNode.next is { } next)
+        {
+            output.AddRange(ReadNodeList(next));
+        }
+
+        return output.ToArray();
+    }
+    
+    private static SharedModels.ListNode CreateListNodeWithValues(int[] values)
+    {
+        switch (values.Length)
+        {
+            case 0:
+                return new SharedModels.ListNode();
+            case 1:
+                return new SharedModels.ListNode(values[0]);
+        }
+
+        var root = new SharedModels.ListNode
+        {
+            val = values[0]
+        };
+
+        var current = root;
+        foreach (var value in values.Skip(1))
+        {
+            var newNode = new SharedModels.ListNode(value);
+            current.next = newNode;
+            current = newNode;
+        }
+
+        return root;
+    }
+}
