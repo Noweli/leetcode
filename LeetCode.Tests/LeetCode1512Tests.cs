85bfd5b359bb16b6966e7f618ab29f081e09a3e4
Patryk Lewon <patryklewonn@gmail.com>
2024-08-26 18:27:51 +0200

    Problem 1512

---
+++ LeetCode.Tests/LeetCode1512Tests.cs
@@ -0,0 +1,20 @@
+﻿using FluentAssertions;
+
+namespace LeetCode.Tests;
+
+[TestFixture]
+public class LeetCode1512Tests
+{
+    [TestCase(new[] { 1, 2, 3, 1, 1, 3 }, 4)]
+    [TestCase(new[] { 1,1,1,1 }, 6)]
+    [TestCase(new[] { 1,2,3 }, 0)]
+    public void NumIdenticalPairs_ReturnExpected(int[] nums, int expected)
+    {
+        // Arrange
+        // Act
+        var result = LeetCode1512.NumIdenticalPairs(nums);
+        
+        // Assert
+        result.Should().Be(expected);
+    }
+}
---
+++ LeetCode/LeetCode1512.cs
@@ -0,0 +1,26 @@
+﻿namespace LeetCode;
+
+public static class LeetCode1512
+{
+    public static int NumIdenticalPairs(int[] nums)
+    {
+        var goodPairs = 0;
+        List<Task> tasks = [];
+
+        tasks.AddRange(nums.Select((_, i) => i)
+            .Select(currentIndex => Task.Run(() =>
+            {
+                for (var j = currentIndex + 1; j < nums.Length; j++)
+                {
+                    if (nums[currentIndex] == nums[j])
+                    {
+                        goodPairs++;
+                    }
+                }
+            })));
+
+        Task.WhenAll(tasks).Wait();
+
+        return goodPairs;
+    }
+}
