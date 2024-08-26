77436077962a4bd43e37acae427c82d240a20b89
Patryk Lewon <patryklewonn@gmail.com>
2024-08-26 18:01:09 +0200

    Problem 1108

---
+++ LeetCode.Tests/LeetCode1108Tests.cs
@@ -0,0 +1,20 @@
+﻿using FluentAssertions;
+
+namespace LeetCode.Tests;
+
+[TestFixture]
+public class LeetCode1108Tests
+{
+    [TestCase("1.1.1.1", "1[.]1[.]1[.]1")]
+    [TestCase("255.100.50.0", "255[.]100[.]50[.]0")]
+    // ReSharper disable once IdentifierTypo
+    public void DefangIPaddr_ReturnExpected(string address, string expected)
+    {
+        // Arrange
+        // Act
+        var result = LeetCode1108.DefangIPaddr(address);
+        
+        // Assert
+        result.Should().Be(expected);
+    }
+}
---
+++ LeetCode/LeetCode1108.cs
@@ -0,0 +1,10 @@
+﻿namespace LeetCode;
+
+public static class LeetCode1108
+{
+    // ReSharper disable once IdentifierTypo
+    public static string DefangIPaddr(string address)
+    {
+        return address.Replace(".", "[.]");
+    }
+}
