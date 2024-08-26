d72d38af0387ebdd59eb149f88b23d03025b21d6
Patryk Lewon <patryklewonn@gmail.com>
2024-08-26 17:56:03 +0200

    Problem 2011

---
+++ LeetCode.Tests/LeetCode2011Tests.cs
@@ -0,0 +1,20 @@
+﻿using FluentAssertions;
+
+namespace LeetCode.Tests;
+
+[TestFixture]
+public class LeetCode2011Tests
+{
+    [TestCase(new[] {"--X","X++","X++"}, 1)]
+    [TestCase(new[] {"++X","++X","X++"}, 3)]
+    [TestCase(new[] {"X++","++X","--X","X--"}, 0)]
+    public void FinalValueAfterOperations_ReturnsExpected(string[] operations, int expected)
+    {
+        // Arrange
+        // Act
+        var result = LeetCode2011.FinalValueAfterOperations(operations);
+        
+        // Assert
+        result.Should().Be(expected);
+    }
+}
---
+++ LeetCode/LeetCode2011.cs
@@ -0,0 +1,21 @@
+﻿namespace LeetCode;
+
+public static class LeetCode2011
+{
+    public static int FinalValueAfterOperations(string[] operations)
+    {
+        var x = 0;
+        foreach (var operation in operations)
+        {
+            if (operation.Contains('-'))
+            {
+                x--;
+                continue;
+            }
+
+            x++;
+        }
+
+        return x;
+    }
+}
