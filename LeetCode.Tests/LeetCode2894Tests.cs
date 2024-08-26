8cf7a1fc022cbaad749659365cd1e7e2b7481408
Patryk Lewon <patryklewonn@gmail.com>
2024-08-26 22:00:36 +0200

    Problem 2894

---
+++ LeetCode.Tests/LeetCode2894Tests.cs
@@ -0,0 +1,20 @@
+﻿using FluentAssertions;
+
+namespace LeetCode.Tests;
+
+[TestFixture]
+public class LeetCode2894Tests
+{
+    [TestCase(10, 3, 19)]
+    [TestCase(5, 6, 15)]
+    [TestCase(5, 1, -15)]
+    public void DifferenceOfSums(int n, int m, int expected)
+    {
+        // Arrange
+        // Act
+        var result = LeetCode2894.DifferenceOfSums(n, m);
+
+        // Assert
+        result.Should().Be(expected);
+    }
+}
---
+++ LeetCode/LeetCode2894.cs
@@ -0,0 +1,13 @@
+﻿namespace LeetCode;
+
+public static class LeetCode2894
+{
+    public static int DifferenceOfSums(int n, int m)
+    {
+        var range = Enumerable.Range(1, n).ToArray();
+        var divisible = range.Where(element => element % m == 0).ToArray();
+        var notDivisibleSum = range.Except(divisible).Sum();
+
+        return notDivisibleSum - divisible.Sum();
+    }
+}
